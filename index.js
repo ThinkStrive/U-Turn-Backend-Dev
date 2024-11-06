ac306d8031572c584a8b327b6827ea9057888ac7
ThinkStrive <infothinkstrive@gmail.com>
2024-11-06 09:16:06 +0530

    updated

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 import express from "express";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import dotenv from "dotenv";
-// import connectDB from "./DB/connection/Connection.js";
-// import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./DB/connection/Connection.js";
+import authRoutes from "./routes/authRoutes.js";
 
-// dotenv.config();
+dotenv.config();
 
 // Instance of express is created
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 // To parse the json data from req.body
-// app.use(express.json());
+app.use(express.json());
 
 // To parse Form data in the req.body :
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // To parse the cookie :
-// app.use(cookieParser());
+app.use(cookieParser());
 
 // Routes :
-// app.use("/api/vender", authRoutes);
+app.use("/api/vender", authRoutes);
 
 
 app.get('/', (req,res) =>{
@@ -29,26 +29,22 @@ app.get('/', (req,res) =>{
 })
 
 // Common error Handler for all  (Best practise):
-// app.use("/", (err, req, res, next) => {
-//   if (err) {
-//     res.status(500).send("Something went wrong!");
-//   }
-// });
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong!");
+  }
+});
 
 // Database func call Before Server Start Lisiting to the PORT  we need to connect DB :
-// connectDB()
-//   .then(() => {
-//     console.log("Database Connected successfully");
-//     const PORT = process.env.PORT || 5000;
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//     process.exit(1);
-//   });
-
-
-
-app.listen(7060, () => console.log('server is running on', 7060))
+connectDB()
+  .then(() => {
+    console.log("Database Connected successfully");
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
